eba5d556c7e2ebcccb48ff7ce6608f68231cf821
Daniel-Wang <danielw68@gmail.com>
2018-04-13 22:50:01 -0700

    Remove node_modules tracking, now adding them back to project will not track them

--- node_modules/hot-formula-parser/es/evaluate-by-operator/operator/equal.js
+++
@@ -1,7 +0,0 @@
-export var SYMBOL = '=';
-
-export default function func(exp1, exp2) {
-  return exp1 === exp2;
-}
-
-func.SYMBOL = SYMBOL;
--- node_modules/react-handsontable/src/index.js
+++
@@ -1,2 +0,0 @@
-module.exports = require('./react-handsontable.jsx').default;
-
