7ba468d445cbec7c5f8ba4ebfbfdeff6b9042228
jhunter5811 <justin.edward.hunter@gmail.com>
2018-09-21 20:51:39 -0500

    Minor updates and tests before release

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
 import Header from './Header';
-var elmnt1;
-var elemt2;
+
+
 
 export default class Calendar extends Component {
-
-  componentDidMount() {
-    elmnt1 = document.getElementById("outer").offsetHeight;
-    elemt2 = document.getElementById("inner").offsetHeight;
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    }
   }
 
+  onChange = date => this.setState({ date })
+
+
   render() {
-    if(elemt2 > elmnt1) {
-      alert('Bang!')
-    }
 
     return(
       <div>
         <Header />
-        <div id="outer" className="card full-page">
-          <textarea className="materialize-textarea" id="inner">
-          </textarea>
+        <div className="main">
+      
         </div>
       </div>
     )
