d1dc5192f42fd168def604d8913644049623e810
jhunter5811 <justin.edward.hunter@gmail.com>
2018-10-22 15:06:06 -0500

    Fixes untracked files

--- src/components/helpers/dev.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  stripePubKey: "pk_test_f017dq0VLdzf22LXPBsLvuDO",
-  MEDIUM_CLIENT_ID_DEV: "e11c36df2413",
-  SLACK_CLIENT_ID_DEV: "359249334709.435757828275",
-  GOOGLE_CLIENT_ID: "1002057797312-63aos1ttm84otuu6ak77atr8gt62g12u.apps.googleusercontent.com"
-};
