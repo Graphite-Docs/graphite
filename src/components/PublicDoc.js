02261cfccc63c623b26af09f8c2a24d15999c7c7
jhunter5811 <justin.edward.hunter@gmail.com>
2018-10-28 15:41:20 -0500

    Fixed public doc UI

--- src/components/PublicDoc.js
+++ src/components/PublicDoc.js
@@ -14,12 +14,28 @@ export default class PublicDoc extends Component {
   }
 
   render() {
+    let toolbar = window.document.getElementsByClassName('ql-toolbar');
+    if(toolbar[0]) {
+      toolbar[0].style.top = "63px";
+    }
     return (
       <div>
       <nav className="navbar-fixed toolbar">
         <div className="nav-wrapper">
           <a href="/" className="left brand-logo text-white">Graphite.<img className="pencil" src="https://i.imgur.com/2diRYIZ.png" alt="pencil" /></a>
           <ul id="nav-mobile" className="right">
+            <li>
+            {
+              this.props.readOnly === true ?
+              <a>
+                Read-Only
+              </a>
+              :
+              <a>
+                Editable
+              </a>
+            }
+            </li>
             <li>
               <a href="http://graphitedocs.com" target="_blank" rel="noopener noreferrer">About Graphite</a>
             </li>
@@ -36,45 +52,12 @@ export default class PublicDoc extends Component {
                 {
                   this.props.singleDocIsPublic === true ?
                   <div>
-                    {
-                      this.props.title === "Untitled" ?
-                      <textarea
-                        className="doc-title materialize-textarea"
-                        placeholder="Give it a title"
-                        type="text"
-                        onChange={this.props.handlePubTitleChange}
-                      />
-                      :
-                      <textarea
-                        className="doc-title materialize-textarea"
-                        placeholder="Title"
-                        type="text"
-                        value={this.props.title}
-                        onChange={this.props.handlePubTitleChange}
-                      />
-                    }
 
-                    {
-                      this.props.readOnly === true ?
-                      <div>
-                        <p style={{textAlign: "center"}}>
-                          <span style={{padding: "5px", boxShadow: "0 4px 6px 0 hsla(240, 100%, 50%, 0.2)"}}>
-                            This collaborative document is currently read-only.
-                          </span>
-                        </p>
-                      </div>
-                      :
-                      <div>
-                        <p style={{textAlign: "center"}}>
-                          <span style={{padding: "5px", boxShadow: "0 4px 6px 0 hsla(120, 100%, 50%, 0.2)"}}>
-                            This collaborative document is editable.
-                          </span>
-                        </p>
-                      </div>
-                    }
+
 
                     {/* <div style={(this.state.readOnly === true) ? {border: "5px solid red"} : null}> */}
                     <div>
+                      <h3 className="center-align">{this.props.title}</h3>
                       <QuillEditorPublic
                         key={this.props.readOnly === true ? "true!" : "false..."} //NOTE: when this.state.readOnly changes in PublicDoc, it will change this key, which will remount QuillEditorPublic, which will remount YjsQuill, so readOnly changes in SingleDoc update PublicDoc text editor...
                         roomId={this.props.idToLoad} //this is a string!
