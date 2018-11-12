90c541615ad2650326f6ef1d813450f529444050
jhunter5811 <justin.edward.hunter@gmail.com>
2018-11-11 19:44:00 -0600

    minor vault fix

--- src/components/helpers/singleVaultFile.js
+++ src/components/helpers/singleVaultFile.js
@@ -15,6 +15,74 @@ let abuf4;
 
 export function loadSingleVaultFile() {
   this.setState({ loading: true });
+  getFile(window.location.href.split('vault/')[1]+ ".json", { decrypt: true })
+    .then(file => {
+      this.setState({
+        file: JSON.parse(file || "{}").name,
+        name: JSON.parse(file || "{}").name,
+        lastModifiedDate: JSON.parse(file || "{}").lastModifiedDate,
+        size: JSON.parse(file || "{}").size,
+        link: JSON.parse(file || "{}").link,
+        type: JSON.parse(file || "{}").type,
+        sharedWith: JSON.parse(file || "{}").sharedWith,
+        tags: JSON.parse(file || "{}").tags
+      });
+      if (this.state.type.includes("word")) {
+        abuf4 = str2ab(this.state.link);
+        mammoth
+          .convertToHtml({ arrayBuffer: abuf4 })
+          .then(result => {
+            var html = result.value; // The generated HTML
+            this.setState({ content: html });
+            console.log(this.state.content);
+            this.setState({ loading: false });
+          })
+          .done();
+      }
+
+      else if (this.state.type.includes("rtf")) {
+        let base64 = this.state.link.split("data:text/rtf;base64,")[1];
+        rtfToHTML.fromString(window.atob(base64), (err, html) => {
+          console.log(window.atob(base64));
+          console.log(html)
+          let htmlFixed = html.replace("body", ".noclass");
+          this.setState({ content:  htmlFixed});
+          this.setState({ loading: "hide", show: "" });
+        })
+      }
+
+      else if (this.state.type.includes("text/plain")) {
+        let base64 = this.state.link.split("data:text/plain;base64,")[1];
+        this.setState({ loading: "hide", show: "" });
+        this.setState({ content: window.atob(base64) });
+      }
+
+      else if (this.state.type.includes("sheet")) {
+        abuf4 = str2ab(this.state.link);
+        var wb = XLSX.read(abuf4, { type: "buffer" });
+        var first_worksheet = wb.Sheets[wb.SheetNames[0]];
+        var data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
+        this.setState({ grid: data });
+        this.setState({ loading: "hide", show: "" });
+      }
+
+      else if (this.state.type.includes("csv")) {
+        let base64 = this.state.link.split("data:text/csv;base64,")[1];
+        this.setState({ grid: Papa.parse(window.atob(base64)).data });
+        this.setState({ loading: "hide", show: "" });
+      }
+
+      else {
+        this.setState({ loading: false });
+      }
+    })
+    .then(() => {
+      this.setState({ loading: false });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
    let files = this.state.files;
    const thisFile = files.find((file) => { return file.id.toString() === window.location.href.split('vault/')[1]}); //this is comparing strings
    console.log("SingleVaultFile - componentDidMount - thisFile is: ", thisFile);
@@ -25,73 +93,7 @@ export function loadSingleVaultFile() {
    }
    this.setState({ index: files.findIndex(findObjectIndex) })
 
-getFile(window.location.href.split('vault/')[1]+ ".json", { decrypt: true })
-  .then(file => {
-    this.setState({
-      file: JSON.parse(file || "{}").name,
-      name: JSON.parse(file || "{}").name,
-      lastModifiedDate: JSON.parse(file || "{}").lastModifiedDate,
-      size: JSON.parse(file || "{}").size,
-      link: JSON.parse(file || "{}").link,
-      type: JSON.parse(file || "{}").type,
-      sharedWith: JSON.parse(file || "{}").sharedWith,
-      tags: JSON.parse(file || "{}").tags
-    });
-    if (this.state.type.includes("word")) {
-      abuf4 = str2ab(this.state.link);
-      mammoth
-        .convertToHtml({ arrayBuffer: abuf4 })
-        .then(result => {
-          var html = result.value; // The generated HTML
-          this.setState({ content: html });
-          console.log(this.state.content);
-          this.setState({ loading: false });
-        })
-        .done();
-    }
 
-    else if (this.state.type.includes("rtf")) {
-      let base64 = this.state.link.split("data:text/rtf;base64,")[1];
-      rtfToHTML.fromString(window.atob(base64), (err, html) => {
-        console.log(window.atob(base64));
-        console.log(html)
-        let htmlFixed = html.replace("body", ".noclass");
-        this.setState({ content:  htmlFixed});
-        this.setState({ loading: "hide", show: "" });
-      })
-    }
-
-    else if (this.state.type.includes("text/plain")) {
-      let base64 = this.state.link.split("data:text/plain;base64,")[1];
-      this.setState({ loading: "hide", show: "" });
-      this.setState({ content: window.atob(base64) });
-    }
-
-    else if (this.state.type.includes("sheet")) {
-      abuf4 = str2ab(this.state.link);
-      var wb = XLSX.read(abuf4, { type: "buffer" });
-      var first_worksheet = wb.Sheets[wb.SheetNames[0]];
-      var data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
-      this.setState({ grid: data });
-      this.setState({ loading: "hide", show: "" });
-    }
-
-    else if (this.state.type.includes("csv")) {
-      let base64 = this.state.link.split("data:text/csv;base64,")[1];
-      this.setState({ grid: Papa.parse(window.atob(base64)).data });
-      this.setState({ loading: "hide", show: "" });
-    }
-
-    else {
-      this.setState({ loading: false });
-    }
-  })
-  .then(() => {
-    this.setState({ loading: false });
-  })
-  .catch(error => {
-    console.log(error);
-  });
 }
 
 export function onDocumentComplete(pages) {
