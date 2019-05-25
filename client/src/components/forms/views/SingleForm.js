889b87612df7adccb9329555138a63aeb6a89a02
Justin Hunter <polluterofminds@Justins-MacBook-Pro.local>
2019-05-25 18:28:54 +0100

    removed team share option from forms

--- client/src/components/forms/views/SingleForm.js
+++ client/src/components/forms/views/SingleForm.js
@@ -1,6 +1,6 @@
 import React, { Component, setGlobal } from 'reactn';
 import { loadForm } from '../helpers/singleForm';
-import { Icon, Input, Container, Popup, Modal, Button, Dropdown, Accordion, Item, TextArea } from 'semantic-ui-react';
+import { Icon, Input, Container, Popup, Modal, Button, TextArea } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import {Menu as MainMenu} from 'semantic-ui-react';
 import EditForm from './EditForm';
@@ -70,9 +70,8 @@ class SingleForm extends Component {
   }
 
   render() {
-      const { userSession, graphitePro, proOrgInfo, singleForm, embed, teamListModalOpen, teamShare, formLoading, formLinkModalOpen, formEmbedModalOpen } = this.global;
-      const { activeItem, activeIndex } = this.state;
-      const teamList = proOrgInfo.teams;
+      const { userSession, proOrgInfo, singleForm, embed, formLoading, formLinkModalOpen, formEmbedModalOpen } = this.global;
+      const { activeItem } = this.state;
       if(formLoading === true) {
         return (
           <SingleFormSkel />
@@ -98,70 +97,6 @@ class SingleForm extends Component {
                 </MainMenu.Item>
                 <MainMenu.Item name='Responses' active={activeItem === 'Responses'} onClick={this.handleItemClick}>
                   <Icon name="comment outline" /> Responses
-                </MainMenu.Item>
-                <MainMenu.Item>
-                  <Dropdown style={{color: "#fff"}} icon='share alternate' simple>
-                    <Dropdown.Menu>
-                    {
-                    graphitePro ?
-                    <Dropdown.Item>
-                        <Modal 
-                          open={teamListModalOpen}
-                          onClose={() => setGlobal({ teamListModalOpen: false})}
-                          closeIcon style={{borderRadius: "0"}}
-                          trigger={<button onClick={() => setGlobal({ teamListModalOpen: true})} className='link-button'>Share With Team</button>}
-                          >
-                          <Modal.Header style={{fontFamily: "Muli, san-serif", fontWeight: "200"}}>Share With Team</Modal.Header>
-                          <Modal.Content>
-                            <Modal.Description>
-                              <p>By sharing with your entire team, each teammate will have immediate access to the form and will be able to make changes.</p>
-                              <p>For reference, you can see your list of teammates by expanding each team below.</p>
-                              <Item.Group divided>
-                              {teamList.map(team => {
-                                  return (
-                                      <Item className="contact-search" key={team.id}>
-                                      <Item.Content verticalAlign='middle'>
-                                      <Accordion>
-                                        <Accordion.Title active={activeIndex === team.id} index={team.id} onClick={this.handleClick}>
-                                          <Icon name='dropdown' />
-                                          {`${team.name} (${team.users.length} members)`}
-                                        </Accordion.Title>
-                                        <Accordion.Content active={activeIndex === team.id}>
-                                          {
-                                            team.users.map(user => {
-                                              return (
-                                                <p key={user.username}>
-                                                  {user.username}
-                                                </p>
-                                              )
-                                            })
-                                          }
-                                        </Accordion.Content>
-                                      </Accordion>
-                                      <br/>
-                                      {
-                                        teamShare === false ? 
-                                        <Button style={{float: "right", borderRadius: "0px"}} secondary onClick={() => single.shareWithTeam({teamId: team.id, teamName: team.name, initialShare: true})}>Share</Button> : 
-                                        <div className="hide" />
-                                      }
-                                      </Item.Content>
-                                      </Item>
-                                      )
-                                    }
-                                  )
-                              }
-                              </Item.Group>
-                              {teamShare === false ? <div className="hide" /> : <Button style={{borderRadius: "0"}}>Sharing...</Button>}
-                            </Modal.Description>
-                          </Modal.Content>
-                        </Modal>
-                        </Dropdown.Item> 
-                      : 
-                      <Dropdown.Item className="hide" />
-                    }
-                    </Dropdown.Menu>
-                  </Dropdown>
-                
                 </MainMenu.Item>
                 <MainMenu.Item>
                   <Modal trigger={
