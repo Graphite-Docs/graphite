4054999a8b6a99a7e3344efe79ef006777825898
polluterofminds <justin@graphitedocs.com>
2019-07-31 15:27:39 -0500

    custom gaia hub

--- src/components/shared/helpers/auth.js
+++ src/components/shared/helpers/auth.js
@@ -1,8 +1,22 @@
+import { getGlobal } from 'reactn';
 const blockstack = require('blockstack');
+  
 export function signIn(e) {
+    const userSession = getGlobal().userSession;
     const origin = window.location.origin;
     e.preventDefault();
-    blockstack.redirectToSignIn(origin, origin + '/manifest.json', ['store_write', 'publish_data', 'email'])
+    const authRequest = blockstack.makeAuthRequest(
+        userSession.generateAndStoreTransitKey(),
+        origin,
+        origin + '/manifest.json',
+        ['store_write', 'publish_data', 'email'],
+        'http://localhost:5000/',
+        blockstack.nextHour().getTime(), {
+          solicitGaiaHubUrl: true
+        } // new options param
+    );
+      
+    userSession.redirectToSignInWithAuthRequest(authRequest);
 }
 
 export function signOut() {
--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import axios from 'axios'
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 
 const userSession = new UserSession({ appConfig })
-console.log(userSession);
 const port = window.location.href.includes('local') ? 'http://localhost:5000' : 'https://whispering-sands-47101.herokuapp.com';
 
 const setAxiosHeaders = () => {
